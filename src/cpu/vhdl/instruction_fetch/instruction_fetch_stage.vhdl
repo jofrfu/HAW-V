36620de8455c5768abfca22c5de031d2e91b1821
Sebastian Brueckner <sebastian.brueckner@haw-hamburg.de>
2017-10-21 16:17:54 +0200

    31 Added file for entity "CPU: Instruction Fetch"
    Just a entity skeleton

---
+++ src/cpu/vhdl/instruction_fetch/instruction_fetch_stage.vhdl
@@ -0,0 +1,14 @@
+--!@file 	instruction_fetch_stage.vhdl
+--!@biref 	This file contains the instruction fetch stage entity of the CPU
+--!@author 	Sebastian Brückner
+--!@date 	2017
+
+--!@biref 	This is the instruction fetch stage of the CPU
+--!@details This Stage sets the programm counterm and loads the new Instructions.
+--!@author 	Sebastian Brückner
+--!@date 	2017
+entity instruction_fetch is
+	port(
+		 
+	);
+end entity instruction_fetch;
