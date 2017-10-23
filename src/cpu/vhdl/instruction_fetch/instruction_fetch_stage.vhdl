6b8e296bed42974a729e84a27345624575f93a37
thismath <matthis.keppner@haw-hamburg.de>
2017-10-23 10:33:27 +0200

    typos

--- src/cpu/vhdl/instruction_fetch/instruction_fetch_stage.vhdl
+++ src/cpu/vhdl/instruction_fetch/instruction_fetch_stage.vhdl
@@ -10,7 +10,7 @@ library IEEE;
 use WORK.riscv_pack.all;
 
 --!@biref 	This is the instruction fetch stage of the CPU
---!@details This Stage sets the programm counterm and loads the new Instructions.
+--!@details This Stage sets the programm counter and loads the new Instructions.
 --!@author 	Sebastian Brückner
 --!@date 	2017
 entity instruction_fetch is
@@ -18,11 +18,11 @@ entity instruction_fetch is
 		 clk, reset : in std_logic;
 		 
 		 cntrl  : in IF_CNTRL_TYPE; --! Control the operation mode of the PC logic
-		 rel	: in DATA_TYPE;		--! relative branch adress
-		 abso	: in DATA_TYPE;		--! absolute branch adress, or base for relative jump
+		 rel	: in DATA_TYPE;		--! relative branch address
+		 abso	: in DATA_TYPE;		--! absolute branch address, or base for relative jump
 		 ins 	: in DATA_TYPE;		--! the new instruction is loaded from here
 		 
 		 IFR	: out DATA_TYPE;	--! Instruction fetch register contents
-		 pc : out DATA_TYPE;		--! programm counter output
+		 pc     : out DATA_TYPE;	--! programm counter output
 	);
 end entity instruction_fetch;
