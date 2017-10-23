04f554b49daabcad8f7461f4dbe5583a94de0f34
jonasfuhrmann <jofrfu@googlemail.com>
2017-10-23 11:40:13 +0200

    38 register select arc finished "CPU: Register_Select"

--- src/cpu/vhdl/riscv_pack.vhd
+++ src/cpu/vhdl/riscv_pack.vhd
@@ -29,5 +29,8 @@ constant REGISTER_COUNT : natural := 32;
 
 subtype DATA_TYPE is std_logic_vector(DATA_WIDTH-1 downto 0);
 subtype REGISTER_ADDRESS_TYPE is std_logic_vector(REGISTER_ADDRESS_WIDTH-1 downto 0);
+subtype REGISTER_COUNT_WIDTH is std_logic_vector(REGISTER_COUNT-1 downto 0);
+
+type reg_out_type is array(REGISTER_COUNT-1 downto 0) of DATA_TYPE;
 
 end riscv_pack;
