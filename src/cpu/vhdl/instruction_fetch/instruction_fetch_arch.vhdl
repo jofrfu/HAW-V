1fe291ce65830cf6fa0477f8479ae27b6ca2c242
Sebastian Brueckner <sebastian.brueckner@haw-hamburg.de>
2017-10-29 15:32:22 +0100

    31 Added entity PC_log "CPU_Instruction Fetch"

--- src/cpu/vhdl/instruction_fetch/instruction_fetch_arch.vhdl
+++ src/cpu/vhdl/instruction_fetch/instruction_fetch_arch.vhdl
@@ -1,12 +1,12 @@
---!@file 	instruction_fetch_arch.vhdl
---!@brief 	Contains Standard architecture for instruction_fetch
---!@author 	Sebastian Brückner
---!@date 	2017
+--!@file     instruction_fetch_arch.vhdl
+--!@brief    Contains Standard architecture for instruction_fetch
+--!@author   Sebastian Brückner
+--!@date     2017
 
---!@brief 	Programm counter of the CPU
+--!@brief   Programm counter of the CPU
 --!@details Contains the Programm counter logic
---!@author 	Sebastian Brückner
---!@date 	2017
+--!@author  Sebastian Brückner
+--!@date    2017
 
 --!@brief   added PC register to the stage
 --!@details was needed to realize the AUIPC instruction
@@ -14,11 +14,11 @@
 --!@date    2017
 
 architecture std_impl of instruction_fetch is 
-	signal IFR_cs : INSTRUCTION_BIT_TYPE := (others => '0');
-	signal PC_cs  : ADDRESS_TYPE := (others => '0');
-	signal PC_ns  : ADDRESS_TYPE;
-	
-	component PC_log is
+    signal IFR_cs : INSTRUCTION_BIT_TYPE := (others => '0');
+    signal PC_cs  : ADDRESS_TYPE := (others => '0');
+    signal PC_ns  : ADDRESS_TYPE;
+    
+    component PC_log is
     port(
          clk, reset : in std_logic;
          
@@ -29,7 +29,9 @@ architecture std_impl of instruction_fetch is
          pc     : out ADDRESS_TYPE     --! programm counter output
     );
     end component PC_log;
-	
+    
+    for all: PC_log use entity work.PC_log(std_impl);
+    
 begin
 
     PC_log_i : PC_log
@@ -42,22 +44,22 @@ begin
         pc => pc_ns
     );
     
-	reg : 
-	process(clk, reset) is
-	begin
-		if clk'event and clk = '1' then
-	        if reset = '1' then
-	            IFR_cs <= (others => '0');
-	            PC_cs <= (others => '0');
-	        else
-	            IFR_cs <= ins;  --store data at rising edge
-	            PC_cs <= PC_ns;    --store actual PC in pc register
-	        end if;
-	    end if; 
-	end process reg;
-		
-	IFR <= IFR_cs;    
-	pc_reg <= PC_cs;   --clocked signal to PC register output of IF stage to ID Stage
-	pc <= pc_ns;       --unclocked signal to memory in IF stage
-	
+    reg : 
+    process(clk, reset) is
+    begin
+        if clk'event and clk = '1' then
+            if reset = '1' then
+                IFR_cs <= (others => '0');
+                PC_cs <= (others => '0');
+            else
+                IFR_cs <= ins;  --store data at rising edge
+                PC_cs <= PC_ns;    --store actual PC in pc register
+            end if;
+        end if; 
+    end process reg;
+        
+    IFR <= IFR_cs;    
+    pc_reg <= PC_cs;   --clocked signal to PC register output of IF stage to ID Stage
+    pc <= pc_ns;       --unclocked signal to memory in IF stage
+    
 end architecture std_impl;
