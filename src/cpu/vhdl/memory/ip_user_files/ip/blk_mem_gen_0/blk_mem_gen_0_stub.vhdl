e8eba7fa30bab80c3150e6f280c118162a05db82
thismath <matthis.keppner@haw-hamburg.de>
2017-11-14 12:11:35 +0100

    42 Correction of generation "CPU: Memory"

--- src/cpu/vhdl/memory/ip_user_files/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
+++ src/cpu/vhdl/memory/ip_user_files/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2017.3 (win64) Build 2018833 Wed Oct  4 19:58:22 MDT 2017
--- Date        : Tue Nov 14 11:40:22 2017
+-- Date        : Tue Nov 14 12:08:54 2017
 -- Host        : LAB28 running 64-bit Service Pack 1  (build 7601)
 -- Command     : write_vhdl -force -mode synth_stub
 --               z:/sem5/achne/repo/ach-ne-2017-2018/src/cpu/vhdl/memory/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
@@ -15,21 +15,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity blk_mem_gen_0 is
   Port ( 
     clka : in STD_LOGIC;
-    rsta : in STD_LOGIC;
     ena : in STD_LOGIC;
     wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
     addra : in STD_LOGIC_VECTOR ( 31 downto 0 );
     dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
     douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
     clkb : in STD_LOGIC;
-    rstb : in STD_LOGIC;
     enb : in STD_LOGIC;
     web : in STD_LOGIC_VECTOR ( 3 downto 0 );
     addrb : in STD_LOGIC_VECTOR ( 31 downto 0 );
     dinb : in STD_LOGIC_VECTOR ( 31 downto 0 );
-    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 );
-    rsta_busy : out STD_LOGIC;
-    rstb_busy : out STD_LOGIC
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
   );
 
 end blk_mem_gen_0;
@@ -38,7 +34,7 @@ architecture stub of blk_mem_gen_0 is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],clkb,rstb,enb,web[3:0],addrb[31:0],dinb[31:0],doutb[31:0],rsta_busy,rstb_busy";
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],clkb,enb,web[3:0],addrb[31:0],dinb[31:0],doutb[31:0]";
 attribute x_core_info : string;
 attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_0,Vivado 2017.3";
 begin
