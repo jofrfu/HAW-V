fbdef49355c2f8a95d9131447e9b095ecf3e2448
thismath <matthis.keppner@haw-hamburg.de>
2017-11-14 10:43:22 +0100

    snap

---
+++ src/cpu/vhdl/memory/memory.vhdl
@@ -0,0 +1,8 @@
+--! @brief register_select.vhd
+--! @author Jonas Fuhrmann, Matthis Keppner
+--! project: ach ne! @ HAW-Hamburg
+
+use WORK.riscv_pack.all;
+library IEEE;
+    use IEEE.std_logic_1164.all;
+    use IEEE.numeric_std.all;
