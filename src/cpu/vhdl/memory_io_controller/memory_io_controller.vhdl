d1ee65ecb11c976a6e141c63eb6530159545495d
Jonas Fuhrmann <jonas.fuhrmann@haw-hamburg.de>
2017-12-02 15:32:06 +0100

    78 big to little endian "CPU: Big Endian to little Endian? "

--- src/cpu/vhdl/memory_io_controller/memory_io_controller.vhdl
+++ src/cpu/vhdl/memory_io_controller/memory_io_controller.vhdl
@@ -31,7 +31,13 @@ architecture beh of memory_io_controller is
 
     signal BYTE_WRITE_EN_s      : std_logic_vector(3 downto 0);
     signal DO_s                 : DATA_TYPE;
-    signal DOB_s                : DATA_TYPE;
+    signal DOB_s                : DATA_TYPE;    
+    
+    -- Little Endian signals
+    signal DO_LITTLE_s             : DATA_TYPE;
+    signal DOB_LITTLE_s            : DATA_TYPE;
+    signal instruction_little_s    : INSTRUCTION_BIT_TYPE;
+    signal DIN_LITTLE_s            : DATA_TYPE;
     
     component memory is
         Port ( 
@@ -83,13 +89,13 @@ begin
         "0000",             --never write => read only
         pc_asynch,          --address is PC
         (others => '0'),    --READ ONLY, NO WRITE
-        instruction,        --write to instruction ou
+        instruction_little_s,        --write to instruction ou
         CLK,
         ----------------
         mem_en,   --enable when enabled memory (when MSB is 1 is an IO access)
         BYTE_WRITE_EN_s,
         ADDR,
-        DIN,
+        DIN_LITTLE_s,
         DOB_s,
         CLK        
     );
@@ -101,7 +107,7 @@ begin
         io_en, -- peripheral enable
         BYTE_WRITE_EN_s,
         ADDR,
-        DIN,
+        DIN_LITTLE_s,
         DO_s,
         
         -- IO
@@ -109,6 +115,20 @@ begin
         PERIPH_IN,
         PERIPH_OUT
     );
+        
+    -- little to big endian
+    DOB_s <= DOB_LITTLE_s( 7 downto  0) & DOB_LITTLE_s(15 downto  8) &
+             DOB_LITTLE_s(23 downto 16) & DOB_LITTLE_s(31 downto 24);
+    
+    DO_s  <= DO_LITTLE_s( 7 downto  0) & DO_LITTLE_s(15 downto  8) &
+             DO_LITTLE_s(23 downto 16) & DO_LITTLE_s(31 downto 24);
+    
+    instruction <= instruction_little_s( 7 downto  0) & instruction_little_s(15 downto  8) &
+                   instruction_little_s(23 downto 16) & instruction_little_s(31 downto 24);
+    
+    -- big to little endian
+    DIN_LITTLE_s <= DIN( 7 downto  0) & DIN(15 downto  8) &
+                    DIN(23 downto 16) & DIN(31 downto 24);
     
     dout_mux:
     process(DOB_s, DO_s, ADDR(ADDR'high)) is
