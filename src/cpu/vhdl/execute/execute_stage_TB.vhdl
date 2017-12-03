f774cea57da8c48b83cecd3c7c65b77b9cc1cfce
Felix_the_Cat <felix.lorenz@outlook.com>
2017-12-03 17:56:19 +0100

    33 execute_TB updated "CPU: Execute Stufe"

--- src/cpu/vhdl/execute/execute_stage_TB.vhdl
+++ src/cpu/vhdl/execute/execute_stage_TB.vhdl
@@ -8,10 +8,10 @@
 --!@author 	Felix Lorenz
 --!@date 	2017
 
-
+use WORK.riscv_pack.all;
 library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_Std.all;
+    use IEEE.Std_logic_1164.all;
+    use IEEE.Numeric_Std.all;
 
 entity execute_stage_tb is
 end entity execute_stage_tb;
@@ -20,7 +20,7 @@ architecture beh of execute_stage_tb is
 
   component execute_stage
       port(
-          clk, reset : in std_logic;
+          clk, reset    : in std_logic;
           WB_CNTRL_IN   : in WB_CNTRL_TYPE;
           MA_CNTRL_IN   : in MA_CNTRL_TYPE;
           EX_CNTRL_IN   : in EX_CNTRL_TYPE;
@@ -29,19 +29,21 @@ architecture beh of execute_stage_tb is
           OPA           : in DATA_TYPE;
           DO_IN         : in DATA_TYPE;
           PC_IN         : in ADDRESS_TYPE;
-          WB_CNTRL_OUT  : out WB_CNTRL_TYPE;
-          MA_CNTRL_OUT  : out MA_CNTRL_TYPE;
-          WORD_CNTRL_OUT: out WORD_CNTRL_TYPE;
-          SIGN_EN       : out std_logic;
-          RESU_DAR      : out DATA_TYPE;
-          Branch        : out std_logic;
-          ABS_OUT       : out DATA_TYPE;
-          REL_OUT       : out DATA_TYPE;
-          DO_OUT        : out DATA_TYPE;
-          PC_OUT        : out ADDRESS_TYPE
+          WB_CNTRL_OUT          : out WB_CNTRL_TYPE;
+          MA_CNTRL_OUT_SYNCH    : out MA_CNTRL_TYPE;
+          MA_CNTRL_OUT_ASYNCH   : out MA_CNTRL_TYPE;
+          WORD_CNTRL_OUT_SYNCH  : out WORD_CNTRL_TYPE;
+          WORD_CNTRL_OUT_ASYNCH : out WORD_CNTRL_TYPE;
+          SIGN_EN               : out std_logic;
+          RESU_DAR_SYNCH        : out DATA_TYPE;
+          RESU_DAR_ASYNCH       : out DATA_TYPE;
+          Branch                : out std_logic;
+          ABS_OUT               : out DATA_TYPE;
+          REL_OUT               : out DATA_TYPE;
+          DO_OUT                : out DATA_TYPE;
+          PC_OUT                : out ADDRESS_TYPE
       );
   end component;
-  for all : execute_stage use entity work.execute_stage(beh);
 
   signal clk, reset: std_logic;
   signal WB_CNTRL_IN: WB_CNTRL_TYPE;
@@ -53,10 +55,13 @@ architecture beh of execute_stage_tb is
   signal DO_IN: DATA_TYPE;
   signal PC_IN: ADDRESS_TYPE;
   signal WB_CNTRL_OUT: WB_CNTRL_TYPE;
-  signal MA_CNTRL_OUT: MA_CNTRL_TYPE;
-  signal WORD_CNTRL_OUT: WORD_CNTRL_TYPE;
+  signal MA_CNTRL_OUT_SYNCH: MA_CNTRL_TYPE;
+  signal MA_CNTRL_OUT_ASYNCH: MA_CNTRL_TYPE;
+  signal WORD_CNTRL_OUT_SYNCH: WORD_CNTRL_TYPE;
+  signal WORD_CNTRL_OUT_ASYNCH: WORD_CNTRL_TYPE;
   signal SIGN_EN: std_logic;
-  signal RESU_DAR: DATA_TYPE;
+  signal RESU_DAR_SYNCH: DATA_TYPE;
+  signal RESU_DAR_ASYNCH: DATA_TYPE;
   signal Branch: std_logic;
   signal ABS_OUT: DATA_TYPE;
   signal REL_OUT: DATA_TYPE;
@@ -68,29 +73,35 @@ architecture beh of execute_stage_tb is
 
 begin
 
-    dut : execute_stage 
-    port map ( 
-        clk            => clk,
-        reset          => reset,
-        WB_CNTRL_IN    => WB_CNTRL_IN,
-        MA_CNTRL_IN    => MA_CNTRL_IN,
-        EX_CNTRL_IN    => EX_CNTRL_IN,
-        Imm            => Imm,
-        OPB            => OPB,
-        OPA            => OPA,
-        DO_IN          => DO_IN,
-        PC_IN          => PC_IN,
-        WB_CNTRL_OUT   => WB_CNTRL_OUT,
-        MA_CNTRL_OUT   => MA_CNTRL_OUT,
-        WORD_CNTRL_OUT => WORD_CNTRL_OUT,
-        SIGN_EN        => SIGN_EN,
-        RESU_DAR       => RESU_DAR,
-        Branch         => Branch,
-        ABS_OUT        => ABS_OUT,
-        REL_OUT        => REL_OUT,
-        DO_OUT         => DO_OUT,
-        PC_OUT         => PC_OUT 
-    );
+    uut: 
+    execute_stage 
+    port map 
+        (   clk                   => clk,
+            reset                 => reset,
+            WB_CNTRL_IN           => WB_CNTRL_IN,
+            MA_CNTRL_IN           => MA_CNTRL_IN,
+            EX_CNTRL_IN           => EX_CNTRL_IN,
+            Imm                   => Imm,
+            OPB                   => OPB,
+            OPA                   => OPA,
+            DO_IN                 => DO_IN,
+            PC_IN                 => PC_IN,
+            WB_CNTRL_OUT          => WB_CNTRL_OUT,
+            MA_CNTRL_OUT_SYNCH    => MA_CNTRL_OUT_SYNCH,
+            MA_CNTRL_OUT_ASYNCH   => MA_CNTRL_OUT_ASYNCH,
+            WORD_CNTRL_OUT_SYNCH  => WORD_CNTRL_OUT_SYNCH,
+            WORD_CNTRL_OUT_ASYNCH => WORD_CNTRL_OUT_ASYNCH,
+            SIGN_EN               => SIGN_EN,
+            RESU_DAR_SYNCH        => RESU_DAR_SYNCH,
+            RESU_DAR_ASYNCH       => RESU_DAR_ASYNCH,
+            Branch                => Branch,
+            ABS_OUT               => ABS_OUT,
+            REL_OUT               => REL_OUT,
+            DO_OUT                => DO_OUT,
+            PC_OUT                => PC_OUT 
+        )
+    ;
+
 
     stimulus: 
     process
@@ -98,11 +109,12 @@ begin
 
         -- Reset
         stop_the_clock <= false; --just for visibility
-        reset <= '1'; wait for clk'event and clk = '1';
-        wait for clk'event and clk = '1';
+        reset <= '1'; wait until clk'event and clk = '1';
+        wait until clk'event and clk = '1';
 
         -- Test starts here
 
+        -- Test ends here
         stop_the_clock <= true;
         wait;
     end process;
