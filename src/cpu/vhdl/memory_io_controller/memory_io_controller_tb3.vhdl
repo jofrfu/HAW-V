03f355e30f685ce316d17a0b2fa93f7e05340a36
Felix_the_Cat <felix.lorenz@outlook.com>
2017-12-30 10:41:10 +0100

    94 memory_io_controller test for peripherals in tb3

--- src/cpu/vhdl/memory_io_controller/memory_io_controller_tb3.vhdl
+++ src/cpu/vhdl/memory_io_controller/memory_io_controller_tb3.vhdl
@@ -1,16 +1,16 @@
---!@brief 	This file contains memory/IO controller bench for checking memory
+--!@brief 	This file contains memory/IO controller bench for checking peripherals
 --!@author 	Felix Lorenz
---!@date 	21.12.2017
+--!@date 	30.12.2017
 
 use WORK.riscv_pack.all;
 library IEEE;
     use IEEE.std_logic_1164.all;
     use IEEE.numeric_std.all;
     
-entity memory_io_controller_TB_mem is
-end entity memory_io_controller_TB_mem;
+entity memory_io_controller_TB_IO is
+end entity memory_io_controller_TB_IO;
 
-architecture TB of memory_io_controller_TB_mem is
+architecture TB of memory_io_controller_TB_IO is
 
 component dut is
     port(
@@ -33,7 +33,7 @@ component dut is
     );
 end component dut;
 for all : dut use entity work.memory_io_controller(beh);
---use xilinx architecture in memory_io_controller for component blk_mem_gen_0_wrapper and load the test_memory.coe
+--use i.e. dummy_bmem architecture in memory_io_controller for component blk_mem_gen_0_wrapper and load the test_memory.coe
 
     signal CLK: STD_LOGIC;
     signal reset: STD_LOGIC;
@@ -128,26 +128,35 @@ begin
         end testWriteMemory;
         
         variable adrVal : integer range 0 to 256;
-        
-    begin
+        variable address31Bits : std_logic_vector(31 downto 0); --need helper variable because it is not possible to have integers greater than 2147483647
+
+        begin
         simulation_running <= true;
+        
+        --testing the memory mapped I/O
         reset <= '1';
         wait until '1'=CLK and CLK'event;
         wait until '1'=CLK and CLK'event;
         reset <= '0';
         
-        --READ Tests
-        --note: the .coe-file contains data where every byte has the value of its address, defined from 0x0 to 0xFF
-        --      when address is 0xAB, the value of the byte is 0xAB
         
         report "#################################################";
-        report "##>>>>>>>>>>>>>>START MEMORY TEST<<<<<<<<<<<<<<##";
+        report "##>>>>>>>>>>>>>>>>START I/O TEST<<<<<<<<<<<<<<<##";
         report "#################################################";
         report "##>>>>>>>>>>>>>>>>>>READ TESTS<<<<<<<<<<<<<<<<<##";
         
+        --configure preipherals as inputs
+        --every input byte has its address offset from 0x80000000 as value
+        for i in 0 to IO_BYTE_COUNT-1 loop
+            PERIPH_IN_EN(i) <= '1';
+            PERIPH_IN(i)    <= std_logic_vector(to_unsigned(i, BYTE_WIDTH));
+        end loop;
+        
+        --read bytewise
         adrVal := 0;
-        while adrVal < 256 loop --read all bytes
-            readMemory(BYTE, std_logic_vector(to_unsigned(adrVal, ADDRESS_WIDTH)));
+        while adrVal < IO_BYTE_COUNT loop --read all bytes
+            address31Bits := std_logic_vector(to_unsigned(adrVal, ADDRESS_WIDTH));
+            readMemory(BYTE, '1' & address31Bits(30 downto 0));
             wait for 1 ns;
             if DOUT /= std_logic_vector(to_unsigned(adrVal, ADDRESS_WIDTH)) then
                 report "error while reading bytewise" severity error;
@@ -157,10 +166,12 @@ begin
             adrVal := adrVal + 1;
         end loop;
         
+        --read halfwords
         adrVal := 0;
-        while adrVal < 256 loop --loop through all words
+        while adrVal < IO_BYTE_COUNT loop --loop through all words
             for offset in 0 to 2 loop --offset to distinguish between positions in words
-                readMemory(HALF, std_logic_vector(to_unsigned(adrVal+offset, ADDRESS_WIDTH)));
+                address31Bits := std_logic_vector(to_unsigned(adrVal+offset, ADDRESS_WIDTH));
+                readMemory(HALF, '1' & address31Bits(30 downto 0));
                 wait for 1 ns;
                 if DOUT /= x"0000" & std_logic_vector(to_unsigned(adrVal+offset+1, BYTE_WIDTH)) & std_logic_vector(to_unsigned(adrVal+offset, BYTE_WIDTH)) then
                     case offset is
@@ -175,9 +186,11 @@ begin
             adrVal := adrVal + 4;
         end loop;
         
+        --read words
         adrVal := 0;
-        while adrVal < 256 loop --loop through all words
-            readMemory(WORD, std_logic_vector(to_unsigned(adrVal, ADDRESS_WIDTH)));
+        while adrVal < IO_BYTE_COUNT loop --loop through all words
+            address31Bits := std_logic_vector(to_unsigned(adrVal, ADDRESS_WIDTH));
+            readMemory(WORD, '1' & address31Bits(30 downto 0));
             wait for 1 ns;
             if DOUT /= std_logic_vector(to_unsigned(adrVal+3, BYTE_WIDTH)) 
                      & std_logic_vector(to_unsigned(adrVal+2, BYTE_WIDTH)) 
@@ -195,28 +208,26 @@ begin
         report "#################################################";
         report "##>>>>>>>>>>>>>>>>>WRITE TESTS<<<<<<<<<<<<<<<<<##";
         
+        --configure preipherals as inputs
+        for i in 0 to IO_BYTE_COUNT-1 loop
+            PERIPH_IN_EN(i) <= '0';
+        end loop;
+        
         --WRITE Tests
-        testWriteMemory(x"AFFEDEAD", WORD, x"00000A00", "error writing word");         
-        testWriteMemory(x"0000ABBA", HALF, x"00000A00", "error writing halfword; offset = 0");  
-        testWriteMemory(x"00000045", BYTE, x"00000A00", "error writing byte; offset = 0");   
-        testWriteMemory(x"00001234", HALF, x"00000A01", "error writing halfword; offset = 1");
-        testWriteMemory(x"0000D00D", HALF, x"00000A02", "error writing halfword; offset = 2");   
-        testWriteMemory(x"00000029", BYTE, x"00000A01", "error writing byte; offset = 1");
-        testWriteMemory(x"000000CE", BYTE, x"00000A02", "error writing byte; offset = 2"); 
-        testWriteMemory(x"000000FA", BYTE, x"00000A03", "error writing byte; offset = 3");         
-        
-        
-        wait until '1' = CLK and CLK'event;
-        wait until '1' = CLK and CLK'event;
-        wait until '1' = CLK and CLK'event;
+        testWriteMemory(x"AFFEDEAD", WORD, x"80000030", "error writing word");         
+        testWriteMemory(x"0000ABBA", HALF, x"80000030", "error writing halfword; offset = 0");  
+        testWriteMemory(x"00000045", BYTE, x"80000030", "error writing byte; offset = 0");   
+        testWriteMemory(x"00001234", HALF, x"80000031", "error writing halfword; offset = 1");
+        testWriteMemory(x"0000D00D", HALF, x"80000032", "error writing halfword; offset = 2");   
+        testWriteMemory(x"00000029", BYTE, x"80000031", "error writing byte; offset = 1");
+        testWriteMemory(x"000000CE", BYTE, x"80000032", "error writing byte; offset = 2"); 
+        testWriteMemory(x"000000FA", BYTE, x"80000033", "error writing byte; offset = 3");      
         
         report "#################################################";
         report "##>>>>>>>>>>>>WRITE TEST SUCCESSFUL<<<<<<<<<<<<##";
         report "#################################################";
-        report "##>>>>>>>>>>>>MEMORY TEST SUCCESSFUL<<<<<<<<<<<##";
+        report "##>>>>>>>>>>>>>I/O TEST SUCCESSFUL<<<<<<<<<<<<<##";
         report "#################################################";
-        report "";
-        
         
         simulation_running <= false;
         wait;
